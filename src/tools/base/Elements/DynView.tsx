fbbe0e014bf31d11c79488113c53e083120e2a0f
Carlos A. <carlosvsk@gmail.com>
2025-02-14 15:48:41 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/DynView.tsx
+++ src/tools/base/Elements/DynView.tsx
@@ -1,14 +1,6 @@
 
 // ---------- import Packs
 import React, { useEffect, useState } from 'react';
-import { Pressable, View } from 'react-native';
-
-// ---------- import Local Tools
-import { argSel, getStlValues, mapElements, pathSel } from '../project';
-import { useData } from '../../..';
-
-export const css =
-  'color: lightblue; background-color: black; font-size: 11px; padding: 2px 6px; border-radius: 3px';
 
 type Tprops = {
   pass: {
@@ -18,19 +10,6 @@ type Tprops = {
     childrenItems: any;
     args: any;
   };
-};
-
-export const processFunctions = async (arr: any[]) => {
-  const defaultVal = { trigger: '', arrFunctions: [] };
-
-  for (const fn of arr) {
-    if (typeof fn === 'function') {
-      const result = await fn();
-      return result || defaultVal;
-    }
-  }
-
-  return defaultVal;
 };
 
 // DynView / BOX
