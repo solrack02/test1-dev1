7dbbb9f4b51b67da030ebe5923845aa325f0c5b8
Carlos A. <carlosvsk@gmail.com>
2025-02-18 12:30:13 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -95,7 +95,7 @@
         , 
 
     (...args:any) => <Elements.ImageBox pass={{
-      elementsProperties:[`{ resizeMode: "contain" }`],
+      elementsProperties:[`{ resizeMode: "cover" }`],
 
       styles:[`{ width: '100%', height: 100 }`],
 
