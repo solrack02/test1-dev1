c4198d4e132b387c678c8306426347a67f8b64c9
Carlos A. <carlosvsk@gmail.com>
2025-02-18 14:14:29 -0300

    Commit generated by Flaxboll.

--- src/tools/base/project/goTo.ts
+++ src/tools/base/project/goTo.ts
@@ -1,8 +1,21 @@
 
 // ---------- import Local Tools
-import { useRoutes } from '../../..';
+import { getVarValue } from './getVarValue';
+import { tools } from '../..';
 
-// ---------- set GoTo Router (with All Screens Access)
-export const goTo = (newRoute: string) =>
-  useRoutes.setState({ currRoute: newRoute });
+type Tprops_goTo = {
+  args: any;
+  pass: { path: string };
+};
+export const goTo = async (props: Tprops_goTo) => {
+  // ---------- set Caps Inputs
+  const { args, pass } = props;
+  let { path } = pass;
+  console.log('GOTO', path);
+  const [condVar, varValue] = getVarValue(path, 'noComponent');
+  console.log('GOTO', varValue);
+  if (condVar) return tools.goTo(varValue);
 
+  // ---------- get Function from A_Project Scope
+  if (!condVar) return tools.goTo(path);
+};
