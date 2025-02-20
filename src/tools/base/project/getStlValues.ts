edd27d17bd0ea709c5da676c9d2d42e94e7541ec
Carlos A. <carlosvsk@gmail.com>
2025-02-20 14:00:05 -0300

    Commit generated by Flaxboll.

--- src/tools/base/project/getStlValues.ts
+++ src/tools/base/project/getStlValues.ts
@@ -9,30 +9,33 @@ import { getVarValue } from './getVarValue';
 // ----------- set Style Variable Selection
 export const getStlValues = (arrGetValues: string[]) => {
   console.log('GET_VAR_VALUES', { arrGetValues });
+  const replaceVarsInString = (inputStr: string) => {
+    return inputStr.replace(/$var_[w.]+/g, match => {
+      const [hasVar, varValue] = getVarValue(match, 'noComponent');
+      return hasVar ? String(varValue) : match; // Substitui pelo valor real, ou mantém se não encontrado
+    });
+  };
 
   const arrStyles = arrGetValues.map(value => {
-    try {
-      // Garantir que value é uma string antes de chamar trim()
-      if (typeof value !== 'string') {
-        console.warn('Valor não é uma string, convertendo:', value);
-        value = JSON.stringify(value); // Converte para string se for um objeto
-      }
+    value = replaceVarsInString(value);
 
-      const trimmedString = value.trim(); // Agora é seguro chamar trim()
-
-      // Log para depuração
-      console.log('STRING PARA JSON5:', trimmedString);
-
-      // Tenta converter a string em objeto
-      const parsedObject = JSON5.parse(trimmedString);
-
-      console.log('OBJETO RESULTANTE:', parsedObject);
-
-      return parsedObject; // Retorna o objeto processado
-    } catch (error) {
-      console.error('Erro ao parsear JSON5:', error, 'Valor recebido:', value);
-      return {}; // Retorna um objeto vazio em caso de erro
+    // Garantir que value é uma string antes de chamar trim()
+    if (typeof value !== 'string') {
+      console.warn('Valor não é uma string, convertendo:', value);
+      value = JSON.stringify(value); // Converte para string se for um objeto
     }
+
+    const trimmedString = value.trim(); // Agora é seguro chamar trim()
+
+    // Log para depuração
+    console.log('STRING PARA JSON5:', trimmedString);
+
+    // Tenta converter a string em objeto
+    const parsedObject = JSON5.parse(trimmedString);
+
+    console.log('OBJETO RESULTANTE:', parsedObject);
+
+    return parsedObject; // Retorna o objeto processado
   });
 
   console.log('GET_VAR_VALUES', { arrStyles });
