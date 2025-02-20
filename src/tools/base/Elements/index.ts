4fab36fe02ab295b88d05da5dcf0753bd68e6cfd
Carlos A. <carlosvsk@gmail.com>
2025-02-20 16:23:18 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/index.ts
+++ src/tools/base/Elements/index.ts
@@ -5,3 +5,4 @@ export { DynView } from './DynView';
 export { Screen3 } from './Screen3';
 export { ImageBox } from './ImageBox';
 export { Text } from './Text';
+export { FlatList2 } from './FlatList2';
