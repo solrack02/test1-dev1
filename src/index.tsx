4a4a333f5f2efeccf5e53232dec19ce147cb1183
Carlos A. <carlosvsk@gmail.com>
2025-02-20 15:10:22 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -35,7 +35,7 @@
           screenElements:[
 
           (...args:any) => <Elements.DynView pass={{
-            elementsProperties:['{ disabled: "$var_all.cond1", onHoverIn: "() => console.log('OIiii')" }'],
+            elementsProperties:['{ disabled: "$var_all.cond1", onHoverIn: "() => console.log('OIiii');" }'],
 
             styles:['{ backgroundColor: "$var_all.colors.primary", width: 20, height: 20 }'],
 
